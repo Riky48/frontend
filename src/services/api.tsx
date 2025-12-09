99e54effe38a26cc164c66e53090b5a600585691
Riky Krotter <110312471+Riky48@users.noreply.github.com>
2025-12-09 19:07:41 -0300

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "backend-production-a6eac.up.railway.app",
+  baseURL: "https://backend-production-a6eac.up.railway.app/",
 });
 
 // Interceptor JWT simple
