a054e5b18165ea530d048aea1f55884e69353d9d
Riky Krotter <110312471+Riky48@users.noreply.github.com>
2025-12-09 19:10:39 -0300

    Update Comentarios.tsx

--- src/components/comentarios/Comentarios.tsx
+++ src/components/comentarios/Comentarios.tsx
@@ -25,7 +25,7 @@ export const Comentarios = ({ postId }: Props) => {
     // === Obtener comentarios ===
     const fetchComments = async () => {
         try {
-            const res = await fetch(`https://backend-production-a6eac.up.railway.app//comentarios/${postId}`, {
+            const res = await fetch(`https://backend-production-a6eac.up.railway.app/comentarios/${postId}`, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -62,7 +62,7 @@ export const Comentarios = ({ postId }: Props) => {
         setLoading(true);
 
         try {
-            const res = await fetch("https://backend-production-a6eac.up.railway.app//comentarios", {
+            const res = await fetch("https://backend-production-a6eac.up.railway.app/comentarios", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
